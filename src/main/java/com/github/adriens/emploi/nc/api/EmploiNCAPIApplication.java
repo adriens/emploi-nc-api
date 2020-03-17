79a0a2e490c3d27f443e620b1acdb0c644af6be6
zYmMiJ <43323367+zYmMiJ@users.noreply.github.com>
2020-03-17 13:58:46 +1100

    fix(typo)

--- src/main/java/com/github/adriens/emploi/nc/api/EmploiNCAPIApplication.java
+++ src/main/java/com/github/adriens/emploi/nc/api/EmploiNCAPIApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmploiNcApiApplication {
+public class EmploiNCAPIApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmploiNcApiApplication.class, args);
+		SpringApplication.run(EmploiNCAPIApplication.class, args);
 	}
 
 }
